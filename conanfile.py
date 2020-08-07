ab21d2f1d3dfc5f7ff1869c8f6b511ba839955aa
Diniz, Jaime (I/E-AEV25) <jaime.diniz-miguel-neto@audi.de>
2020-08-07 13:33:16 +0200

    "Update"

--- conanfile.py
+++ conanfile.py
@@ -2,8 +2,8 @@
 from conans import ConanFile, CMake, tools
 import os
 
-class PipelineJobXConan(ConanFile):
-    name = "test"
+class WaterConan(ConanFile):
+    name = "Water"
     user = "aev25"
     version = "0.1.0"
     channel = "stable"
